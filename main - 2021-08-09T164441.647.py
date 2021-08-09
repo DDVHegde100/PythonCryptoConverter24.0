ba39682db1728a8cdcfb9f33cfe72c2206f18f29
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-09 16:46:18 -0400

    Add files via upload

---
+++ main - 2021-08-09T164441.647.py
@@ -0,0 +1,87 @@
+print("Welcome to Dhruv's Cruptocurrency Converter!")
+crypt=str(input('Enter the output cryptocurrency code:'))
+
+if(crypt=='BTC'):
+    USD=float(input('Enter the amount in USD:'))
+    BTC=USD*0.000022
+    print('%0.3f USD is equal to %0.3f bitcoin' %(USD, BTC))
+elif(crypt=='ETH'):
+    USD=float(input('Enter the amount in USD:'))
+    ETH=USD*0.000321
+    print('%0.3f USD is equal to %0.3f ethereum' %(USD, ETH))
+elif(crypt=='LTC'):
+    USD=float(input('Enter the amount in USD:'))
+    LTC=USD*0.0064623264
+    print('%0.3f USD is equal to %0.3f litecoin' %(USD, LTC))
+elif(crypt=='ADA'):
+    USD=float(input('Enter the amount in USD:'))
+    ADA=USD*0.681243
+    print('%0.3f USD is equal to %0.3f cardano' %(USD, ADA))
+elif(crypt=='DOGE'):
+    USD=float(input('Enter the amount in USD:'))
+    DOGE=USD*4.757766
+    print('%0.3f USD is equal to %0.3f dogecoin' %(USD, DOGE))
+elif(crypt=='USDT'):
+    USD=float(input('Enter the amount in USD:'))
+    USDT=USD*1
+    print('%0.3f USD is equal to %0.3f Tether' %(USD, DOGE))
+elif(crypt=='USDC'):
+    USD=float(input('Enter the amount in USD:'))
+    USDC=USD*1.00040016
+    print('%0.3f USD is equal to %0.3f USDC' %(USD, USDC))
+elif(crypt=='DOT'):
+    USD=float(input('Enter the amount in USD:'))
+    DOT=USD*0.0489955904
+    print('%0.3f USD is equal to %0.3f USDC' %(USD, DOT))
+elif(crypt=='UNI'):
+    USD=float(input('Enter the amount in USD:'))
+    UNI=USD*0.0373831776
+    print('%0.3f USD is equal to %0.3f UNI' %(USD, UNI))
+elif(crypt=='WBTC'):
+    USD=float(input('Enter the amount in USD:'))
+    WBTC=USD*0.0000226775316
+    print('%0.3f USD is equal to %0.3f WBTC' %(USD, WBTC))
+elif(crypt=='XLM'):
+    USD=float(input('Enter the amount in USD:'))
+    XLM=USD*3.4106412
+    print('%0.3f USD is equal to %0.3f XLM' %(USD, XLM))
+elif(crypt=='LINK'):
+    USD=float(input('Enter the amount in USD:'))
+    LINK=USD*0.0423908436
+    print('%0.3f USD is equal to %0.3f LINK' %(USD, LINK))
+elif(crypt=='ICP'):
+    USD=float(input('Enter the amount in USD:'))
+    ICP=USD*0.0180603215
+    print('%0.3f USD is equal to %0.3f ICP' %(USD, ICP))
+elif(crypt=='MATIC'):
+    USD=float(input('Enter the amount in USD:'))
+    MATIC=USD*0.900900901
+    print('%0.3f USD is equal to %0.3f MATIC' %(USD, MATIC))
+elif(crypt=='THETA'):
+    USD=float(input('Enter the amount in USD:'))
+    THETA=USD*0.156739812
+    print('%0.3f USD is equal to %0.3f Theta' %(USD, THETA))
+elif(crypt=='FIL'):
+    USD=float(input('Enter the amount in USD:'))
+    FIL=USD*0.0152905199
+    print('%0.3f USD is equal to %0.3f Filecoin' %(USD, FIL))
+elif(crypt=='DAI'):
+    USD=float(input('Enter the amount in USD:'))
+    DAI=USD*0.0152905199
+    print('%0.3f USD is equal to %0.3f Dai' %(USD, DAI))
+elif(crypt=='TRX'):
+    USD=float(input('Enter the amount in USD:'))
+    TRX=USD*14.015417
+    print('%0.3f USD is equal to %0.3f Tron' %(USD, TRX))
+elif(crypt=='EOS'):
+    USD=float(input('Enter the amount in USD:'))
+    EOS=USD*0.221238938
+    print('%0.3f USD is equal to %0.3f Eos' %(USD, EOS))
+elif(crypt=='DCR'):
+    USD=float(input('Enter the amount in USD:'))
+    DCR=USD*0.00640122904
+    print('%0.3f USD is equal to %0.3f DCR' %(USD, DCR))
+elif(crypt=='HBAR'):
+    USD=float(input('Enter the amount in USD:'))
+    HBAR=USD*0.0978502304
+    print('%0.3f USD is equal to %0.3f HBAR' %(USD, HBAR))
